50f010405e6d9b880b9b826e8ec003815858164c
sam4314707@gmail.com <sam4314707@gmail.com>
2022-01-04 01:19:19 +0800

    202201040119

--- InputBoxDemo/InputBoxDemo/Form1.cs
+++ InputBoxDemo/InputBoxDemo/Form1.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace InputBoxDemo
 {
     public partial class Form1 : Form
     {
+        string account, password;
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            label1.Text = "帳號：" + account;
+            label2.Text = "密碼：" + password;
+        }
+
         public Form1()
         {
             InitializeComponent();
+
+            account = Interaction.InputBox("輸入帳號", "登入", "Mary", -1, -1);
+            password = Interaction.InputBox("輸入密碼", "登入", "", -1, -1);
         }
     }
 }
