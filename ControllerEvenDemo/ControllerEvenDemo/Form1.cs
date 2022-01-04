b6997390cd0b83804f14f19201c14ec4c67fef65
sam4314707@gmail.com <sam4314707@gmail.com>
2022-01-05 03:35:13 +0800

    20220105Demo

---
+++ ControllerEvenDemo/ControllerEvenDemo/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControllerEvenDemo
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            label10.Text = e.KeyCode.ToString();
+            label11.Text = e.KeyValue.ToString();
+            label12.Text = e.KeyData.ToString();
+
+            label7.Text = Convert.ToString(++count);
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            label13.Text = Convert.ToString(e.KeyChar);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            label14.Text = Convert.ToString(e.KeyData);
+
+            count = 0;
+        }
+    }
+}
---
+++ ControllerEventPractice/ControllerEventPractice/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControllerEventPractice
+{
+    public partial class Form1 : Form
+    {
+        int width;
+        int height;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyValue == 37)
+            {
+                label1.Left -= 10;
+            }
+
+            if(e.KeyValue == 38)
+            {
+                label1.Top -= 10;
+            }
+
+            if(e.KeyValue == 39)
+            {
+                label1.Left += 10;
+            }
+
+            if(e.KeyValue == 40)
+            {
+                label1.Top += 10;
+            }
+        }
+
+        
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            width = this.Width;
+            height = this.Height;
+            label1.Text ="寬度：" + Convert.ToString(width) + " " + "長度：" + Convert.ToString(height);
+        }
+    }
+}
