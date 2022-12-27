68be408ee65a7856abe6499218bd2c3e7c6f9084
sam4314707 <sam4314707@gmail.com.tw>
2022-12-27 16:22:41 +0800

    Shape Game

---
+++ repos/ShapeNinja/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace ShapeNinja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.ovalShape1 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape2 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.rectangleShape1 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label1.Location = new System.Drawing.Point(673, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.rectangleShape1,
+            this.ovalShape2,
+            this.ovalShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(800, 450);
+            this.shapeContainer1.TabIndex = 1;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // ovalShape1
+            // 
+            this.ovalShape1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ovalShape1.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape1.Location = new System.Drawing.Point(154, 123);
+            this.ovalShape1.Name = "ovalShape1";
+            this.ovalShape1.Size = new System.Drawing.Size(75, 71);
+            this.ovalShape1.Click += new System.EventHandler(this.ovalShape1_Click);
+            // 
+            // ovalShape2
+            // 
+            this.ovalShape2.BorderColor = System.Drawing.Color.Black;
+            this.ovalShape2.FillColor = System.Drawing.Color.Red;
+            this.ovalShape2.FillGradientColor = System.Drawing.Color.Transparent;
+            this.ovalShape2.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.ovalShape2.Location = new System.Drawing.Point(382, 176);
+            this.ovalShape2.Name = "ovalShape2";
+            this.ovalShape2.Size = new System.Drawing.Size(162, 69);
+            this.ovalShape2.Click += new System.EventHandler(this.ovalShape2_Click);
+            // 
+            // rectangleShape1
+            // 
+            this.rectangleShape1.FillColor = System.Drawing.Color.Aqua;
+            this.rectangleShape1.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid;
+            this.rectangleShape1.Location = new System.Drawing.Point(168, 277);
+            this.rectangleShape1.Name = "rectangleShape1";
+            this.rectangleShape1.Size = new System.Drawing.Size(50, 50);
+            this.rectangleShape1.Click += new System.EventHandler(this.rectangleShape1_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape1;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ repos/ShapeNinja/Form1.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualBasic.PowerPacks;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShapeNinja
+{
+
+    public partial class Form1 : Form
+    {
+        Random rd, rd2, rd3;
+        int Score;
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            ovalShape2.Left = rd.Next(0 + ovalShape2.Width, this.Width - ovalShape2.Width);
+            ovalShape2.Top = rd.Next(0 + ovalShape2.Height, this.Height - ovalShape2.Height);
+            timer2.Interval = rd.Next(1000, 3000);
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            rectangleShape1.Left = rd.Next(0 + rectangleShape1.Width, this.Width - rectangleShape1.Width);
+            rectangleShape1.Top = rd.Next(0 + rectangleShape1.Height, this.Height - rectangleShape1.Height);
+            timer3.Interval = rd.Next(1000, 3000);
+        }
+
+        private void ovalShape1_Click(object sender, EventArgs e)
+        {
+            ovalShape1.Left = rd.Next(0 + ovalShape1.Width, this.Width - ovalShape1.Width);
+            ovalShape1.Top = rd.Next(0 + ovalShape1.Height, this.Height - ovalShape1.Height);
+            timer1.Interval = rd.Next(1000, 3000);
+            Score += 5;
+            label1.Text = Score.ToString();
+        }
+
+        private void rectangleShape1_Click(object sender, EventArgs e)
+        {
+            rectangleShape1.Left = rd.Next(0 + rectangleShape1.Width, this.Width - rectangleShape1.Width);
+            rectangleShape1.Top = rd.Next(0 + rectangleShape1.Height, this.Height - rectangleShape1.Height);
+            timer3.Interval = rd.Next(1000, 3000);
+            Score += 5;
+            label1.Text = Score.ToString();
+
+        }
+
+        private void ovalShape2_Click(object sender, EventArgs e)
+        {
+            ovalShape2.Left = rd.Next(0 + ovalShape2.Width, this.Width - ovalShape2.Width);
+            ovalShape2.Top = rd.Next(0 + ovalShape2.Height, this.Height - ovalShape2.Height);
+            timer2.Interval = rd.Next(1000, 3000);
+            Score -= 10;
+            label1.Text = Score.ToString();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            rd = new Random();
+            System.Threading.Thread.Sleep(100);
+            rd2 = new Random();
+            System.Threading.Thread.Sleep(100);
+            rd3 = new Random();
+            System.Threading.Thread.Sleep(100);
+            Refresh();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ovalShape1.Left = rd.Next(0 + ovalShape1.Width, this.Width - ovalShape1.Width);
+            ovalShape1.Top = rd.Next(0 + ovalShape1.Height, this.Height - ovalShape1.Height);
+            timer1.Interval = rd.Next(1000, 3000);
+
+        }
+    }
+}
