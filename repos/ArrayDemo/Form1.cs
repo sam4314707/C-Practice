9e5215676603aeaabb13144fe650d3798722d657
sam4314707 <sam4314707@gmail.com.tw>
2023-03-02 08:35:31 +0800

    2023/3/2

---
+++ repos/ArrayDemo/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayDemo
+{
+    public partial class Form1 : Form
+    {
+        int[] score = new int[5];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
